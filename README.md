4fc3433b0ecf7d2dd6a0fae922fd15eec08583e8
Jeff Jagoda <jjagoda@bandwidth.com>
2014-09-17 13:03:59 -0400

    Basic point creation.

--- README.md
+++ README.md
@@ -8,3 +8,42 @@ outflux
 ## Overview
 
 	npm install outflux
+
+`outflux` provides the ability to send metrics to [InfluxDB][influxdb] from
+[Hapi][hapi] plugins.
+
+	exports.register = function (plugin, options, done) {
+		. . .
+		plugin.plugins.outflux.point("metric", { a : 1, b : 2 });
+		. . .
+	};
+
+## Configuration
+
+The `outflux` plugin can be configured with the following options:
+
+| name | required | description            |
+|------|----------|------------------------|
+| url  | yes      | The Influx series URL. |
+
+The series URL should be the fully qualified URL (including the username and
+password) as specified in the [InfluxDB documentation][influx-queries]. For
+example `http://example.com/db/somedb/series?u=username&p=password`.
+
+## API
+
+### outflux.point (type, data)
+
+| parameter | description                  |
+|-----------|------------------------------|
+| type      | The type of point to create. |
+| data      | The data point.              |
+
+Creates a new data point. The `data` object is a standard JavaScript object.
+The key names are used as the column names for the point. Returns a promise that
+is resolved if the point is successfully processed by Influx and rejected
+otherwise.
+
+[hapi]: http://hapijs.com/ "Hapi"
+[influxdb]: http://influxdb.com/ "InfluxDB"
+[influx-queries]: http://influxdb.com/docs/v0.8/api/reading_and_writing_data.html "InfluxDB API"
--- test/outflux_spec.js
+++ test/outflux_spec.js
@@ -1,11 +1,14 @@
 "use strict";
-var fs      = require("fs");
+var Fs      = require("fs");
+var Hapi    = require("hapi");
 var Lab     = require("lab");
-var outflux = require("../lib/outflux");
-var path    = require("path");
+var Nock    = require("nock");
+var Outflux = require("../lib/outflux");
+var Path    = require("path");
 var Q       = require("q");
 var script  = exports.lab = Lab.script();
 
+var after    = script.after;
 var before   = script.before;
 var describe = script.describe;
 var expect   = Lab.expect;
@@ -15,9 +18,19 @@ describe("The outflux plugin", function () {
 	var manifest;
 
 	before(function (done) {
-		var manifestFile = path.join(__dirname, "..", "package.json");
+		Nock.disableNetConnect();
+		done();
+	});
 
-		Q.ninvoke(fs, "readFile", manifestFile, { encoding : "utf8" })
+	after(function (done) {
+		Nock.enableNetConnect();
+		done();
+	});
+
+	before(function (done) {
+		var manifestFile = Path.join(__dirname, "..", "package.json");
+
+		Q.ninvoke(Fs, "readFile", manifestFile, { encoding : "utf8" })
 		.then(function (contents) {
 			manifest = JSON.parse(contents);
 		})
@@ -25,14 +38,90 @@ describe("The outflux plugin", function () {
 	});
 
 	it("has a name", function (done) {
-		expect(outflux.register.attributes, "name").to.have.property("name", manifest.name);
+		expect(Outflux.register.attributes, "name").to.have.property("name", manifest.name);
 		done();
 	});
 
 	it("has a version", function (done) {
-		expect(outflux.register.attributes, "version")
+		expect(Outflux.register.attributes, "version")
 		.to.have.property("version", manifest.version);
 
 		done();
+	});
+
+	describe("when configured without a database URL", function () {
+		var error;
+		var pack;
+
+		before(function (done) {
+			pack = new Hapi.Pack();
+
+			Q.ninvoke(pack, "register", Outflux)
+			.then(
+				function () {
+					throw new Error("Should not succeed.");
+				},
+				function (failure) {
+					error = failure;
+				}
+			)
+			.nodeify(done);
+		});
+
+		it("fails to register", function (done) {
+			expect(error, "type").to.be.an.instanceOf(Error);
+			expect(error.message, "message").to.match(/url is required/i);
+			done();
+		});
+	});
+
+	describe("when configured correctly", function () {
+		var pack;
+
+		before(function (done) {
+			var plugins = {
+				plugin  : Outflux,
+				options : {
+					url : "http://example.com/db/test/series?u=foo&p=bar"
+				}
+			};
+
+			pack = new Hapi.Pack();
+
+			Q.ninvoke(pack, "register", plugins).nodeify(done);
+		});
+
+		describe("creating a new point", function () {
+			var request;
+
+			before(function (done) {
+				request = new Nock("http://example.com")
+				.post(
+					"/db/test/series?u=foo&p=bar",
+					[
+						{
+							name    : "metric",
+							columns : [ "a", "b" ],
+							points  : [
+								[ 1, 2 ]
+							]
+						}
+					]
+				)
+				.reply(200);
+
+				pack.plugins.outflux.point("metric", { a : 1, b : 2 }).nodeify(done);
+			});
+
+			after(function (done) {
+				Nock.cleanAll();
+				done();
+			});
+
+			it("sends the point to the specified URL", function (done) {
+				expect(request.isDone(), "request").to.be.true;
+				done();
+			});
+		});
 	});
 });
